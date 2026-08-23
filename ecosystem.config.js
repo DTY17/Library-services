a1c9f5939f66fe0e4346e98e19a588dbb3541660
DTY17 <dinanthemika.personal@gmail.com>
2026-08-24 03:46:21 +0530

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,22 @@ module.exports = {
       name: "BookService",
       script: "java",
       args: "-jar BookService/target/BookService-0.0.1-SNAPSHOT.jar",
-      log_file: "./log/BookService.log"
+      log_file: "./log/BookService.log",
+      instances: 2
     },
     {
       name: "RecordService",
       script: "java",
       args: "-jar RecordService/target/RecordServie-0.0.1-SNAPSHOT.jar",
-      log_file: "./log/RecordService.log"
+      log_file: "./log/RecordService.log",
+      instances: 2
     },
     {
       name: "UserService",
       script: "java",
       args: "-jar UserService/target/UserService-0.0.1-SNAPSHOT.jar",
-      log_file: "./log/UserService.log"
+      log_file: "./log/UserService.log",
+      instances: 2
     }
   ]
 }
